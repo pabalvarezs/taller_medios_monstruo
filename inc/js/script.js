c06879e7918b0e7f15a3dc2d1ed31157b593454a
Pablo Alvarez <pabloalvarezseguel@gmail.com>
2020-12-17 16:47:55 -0300

    funcion desaparece imagenes sobre monstruo en papelera

--- inc/js/script.js
+++ inc/js/script.js
@@ -1,1 +1,15 @@
 // inicio script
+
+function drag(event) {
+    event.dataTransfer.setData('Text/html', event.target.id); 
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+function drop(event, target) {
+    event.preventDefault()
+    var data = event.dataTransfer.getData("text/html"); 
+    // console.log(target.id, event.target.id) 
+    document.getElementById(data).style.visibility="hidden"
+}

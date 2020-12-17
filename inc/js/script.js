034692290d6075ef699a4e26089f9e063bd6f68e
Pablo Alvarez <pabloalvarezseguel@gmail.com>
2020-12-17 17:34:18 -0300

    agrego contador a las img arrastrables

--- inc/js/script.js
+++ inc/js/script.js
@@ -7,9 +7,20 @@ function drag(event) {
 function allowDrop(event) {
     event.preventDefault();
 }
+var contador = 1;
+
 function drop(event, target) {
     event.preventDefault()
     var data = event.dataTransfer.getData("text/html"); 
     // console.log(target.id, event.target.id) 
     document.getElementById(data).style.visibility="hidden"
+    console.log(contador)
+    contador += 1;
+    if (contador>6)
+        document.getElementById("monstruo").style.opacity = 1
+    else
+        document.getElementById("monstruo").style.opacity = 1-(1/contador)
+
+
+
 }

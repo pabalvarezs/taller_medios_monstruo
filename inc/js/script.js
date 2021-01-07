708d4c25983a8acd19a8381dcf885fce5068a3a1
Pablo IMAC <pabloalvarezseguel@gmail.com>
2021-01-07 12:46:41 -0300

    agregamos audio y animacion en click con basura limpiada

--- inc/js/script.js
+++ inc/js/script.js
@@ -1,56 +1,63 @@
-// inicio script
-
-
-
+// FUNCION ANIMACION CUANDO BOTAS LA BASURA
+var habilitarAnimacion = false;
+function botonMonstruoLimpio(){
+    if(habilitarAnimacion == true){
+        // ESCONDEMOS LOS OBJETOS DE LA PAG PARA MOSTRA VIDEO
+        let x = document.getElementById("contenedor_paginaweb");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+        // MOSTRAMOS ANIMACION
+        document.getElementById("video_botar_basura").className = "mostrar";
+        document.getElementById("")
+        // REPRODUCIMOS AUDIO
+        let audio3 = document.getElementById("audioParte2");
+        audio3.play();
+        document.getElementById("audioParte2").loop = true; 
+    }
+    else{
+        console.log("ESTA FUNCION SOLO ESTÁ HABILITADA SI LIMPIASTE TODA LA BASURA")
+    }
+}
+// FUNCION ARRASTRAR BASURA
 function drag(event) {
     event.dataTransfer.setData('Text/html', event.target.id); 
 }
-
 function allowDrop(event) {
     event.preventDefault();
 }
+// FUNCION LOGICA CANTIDAD DE BASURA BOTADA
 var contador = 1;
-
-function drop(event, target) {
-    
+function drop(event, target) {    
     event.preventDefault()
     var data = event.dataTransfer.getData("text/html"); 
     // console.log(target.id, event.target.id) 
     document.getElementById(data).style.visibility="hidden";
     console.log(contador);
     contador += 1;
-    if (contador==3){
-        // ESCONDEMOS LOS OBJETOS DE LA PAG PARA MOSTRA VIDEO
-        var x = document.getElementById("contenedor_paginaweb");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
-        // MOSTRAMOS VIDEO DEL MONSTRUO
-        // let imgFondo = new Image;
-        // imgFondo.src ='img/fondos/fondo_segundaparte.gif'
-        // var div = document.getElementById("contenedor_paginaweb");
-        // div.style.backgroundImage = url('imgFondo.src');
-        // document.getElementById("video_botar_basura").style.visibility="visible";
-        // document.getElementById("video_botar_basura").style.display="block";
-        // document.getElementById("video_botar_basura").className = "mostrar";
-        // document.getElementById("video_botar_basura").classList.add('mostrar');
-        // document.getElementById("video_botar_basura").classList.remove('oculto');   
+    if (contador==5){
+        habilitarAnimacion = true;
+        // CAMBIOS DE AUDIO 
+        // REPRODUCIOMOS EFECTO TODA LA BASURA ELIMINADA
+        let audio1 = document.getElementById("basuraEliminada");
+        audio1.play();
+        // CAMBIAMOS AUDIO (PARTE 1) PRINCIPAL EN LOOP DE LA WEB
+        let audio2 = document.getElementById("audio");
+        audio2.pause();
+        audio2.currentTime = 0;
+
+        // MOSTRAMOS CURSOR EN MONSTRUO
+        document.getElementById("divMonstruo").classList.add("cursor");
     }        
     else{
-        // document.getElementById("divMonstruo")
-        // console.log(document.getElementById("divMonstruo"));
-
+        habilitarAnimacion = false;
     }
-    // document.getElementById("divMonstruo").style.opacity = 1-(1/contador)
-    var audio = document.getElementById("botarAudio");
+    // AUDIO BOTAR BASURA
+    let audio = document.getElementById("botarAudio");
     audio.currentTime = 2;
     audio.play();
-    // audio.pause();
-
-
-
-
 }
+ 
 

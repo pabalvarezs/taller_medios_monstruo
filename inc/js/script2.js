5759e7bfb22545ff75ab9d9bc0b0f54086766877
Pablo IMAC <pabloalvarezseguel@gmail.com>
2021-01-10 14:06:33 -0300

    agrego monstruo y zoom

--- inc/js/script2.js
+++ inc/js/script2.js
@@ -1,12 +1,17 @@
 // INICIO JS 2
 // ABRIMOS LOS POPUP`S
+// DEFINIMOS TIEMPO RETRASO ABRIR VENTANAS EN MS
+const tiempo_retraso_ventana = 1000;
 function abrir_ventanas_retraso(){
     setTimeout(function abrir_ventanas(url){
         let myWindow1 = window.open( "popup1.html", "popup1", "width=1280,height=1080, top=250,left=200, menubar=no, scrollbars=no, resizable=no");
         let myWindow2 = window.open( "popup2.html", "popup2", "width=1410,height=680, top=300,left=850, menubar=no, scrollbars=no, resizable=no");
         let myWindow3 = window.open( "popup3.html", "popup3", "width=380,height=500, top=450,left=1500, menubar=no, scrollbars=no, resizable=no");
         let myWindow4 = window.open( "popup4.html", "popup4", "width=565,height=488, top=800,left=500, menubar=no, scrollbars=no, resizable=no");
-    },1000);
+    },tiempo_retraso_ventana);
 }
-abrir_ventanas_retraso();
+// abrir_ventanas_retraso();
+// DEFINIMOS TIEMPO RETRASO ABRIR VENTANAS EN MS
+const tiempo_retraso_monstruo = 1000;
 
+// ------------------------------------------FUNCION ZOOM RUEDA MOUSE-----------------------------------------
